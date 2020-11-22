b5a3fc201d729937d7d2eb0f04167805ffffd3ab
Adarsh Kulkarni <adarsh@ghostrobotics.io>
2020-11-21 20:43:15 -0500

    heightmap creation during random obst gen works

--- obstacles.py
+++ obstacles.py
@@ -3,38 +3,62 @@ import numpy as np
 import meshcat
 import meshcat.geometry as geom
 import meshcat.transformations as tforms
+import cv2
 
 from pydrake.all import *
 
 
 class Obstacles:
     def __init__(self, N=5, multi_constraint=False):
-        self.xy_offset = [.25, .2]
-        # print("OBST n", N)
-        self.cubes = self.gen_rand_obst_cubes(N)
-        self.cubes = self.get_known_cubes()
-        print("CUBES", self.cubes)
+        self.xy_offset = [.15, 0.1]
+        self.roi_dims = np.array([0.25, 0.25])
         self.multi_constraint = multi_constraint
 
-    def gen_rand_obst_cubes(self, N, roi_dims=(0.15, 0.15), min_size=0.02, max_size=0.05):
+        self.voxels_per_meter = 200  # 0.5cm resolution
+        self.heightmap = np.zeros(np.round(self.roi_dims * self.voxels_per_meter).astype(np.int))
+
+        self.cubes = self.gen_rand_obst_cubes(N, 0.02, 0.1)
+        # self.cubes = self.get_known_cubes()
+
+        self.heightmap = cv2.rotate(self.heightmap, cv2.ROTATE_180)
+
+        self.visualize_heightmap()
+
+    def gen_rand_obst_cubes(self, N, min_size=0.02, max_size=0.05):
         obst = []
         for i in range(N):
             size = np.random.rand() * (max_size - min_size) + min_size
 
-            loc_x = np.random.rand() * (roi_dims[0] - size) + self.xy_offset[0]
-            loc_y = np.random.rand() * (roi_dims[1] - size) + self.xy_offset[1]
-            # loc_x = self.xy_offset[0]
-            # loc_y = self.xy_offset[1]
+            loc_x = np.random.rand() * (self.roi_dims[0] - size) + size / 2
+            loc_y = np.random.rand() * (self.roi_dims[1] - size) + size / 2
+
+            hmap_x = int(loc_x * self.voxels_per_meter)  # + self.roi_dims[0] * self.voxels_per_meter / 2)
+            hmap_y = int(loc_y * self.voxels_per_meter)  # + self.roi_dims[1] * self.voxels_per_meter / 2)
+            half_size_hmap = int(size * self.voxels_per_meter / 2)
+            print("loc_x", loc_x, "loc_y", loc_y, "hmap_x", hmap_x, "hmap_y", hmap_y, "half_size_hmap", half_size_hmap)
+
+            for r in range(hmap_x - half_size_hmap, hmap_x + half_size_hmap):
+                for c in range(hmap_y - half_size_hmap, hmap_y + half_size_hmap):
+                    if size > self.heightmap[r, c]:
+                        self.heightmap[r, c] = size
+
+            loc_x += self.xy_offset[0]
+            loc_y += self.xy_offset[1]
 
             obst.append((loc_x, loc_y, size))
         return obst
 
+    def visualize_heightmap(self):
+        disp_image = cv2.normalize(self.heightmap, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8UC1)
+        disp_image = cv2.resize(disp_image, (200, 200), interpolation=cv2.INTER_AREA)
+        cv2.imshow("test", disp_image)
+        cv2.waitKey(1000)
+
     def get_known_cubes(self):
-        return [(0.25, 0.1, 0.2)]
+        return [(0.25, 0.1, 0.2), (0.25, 0.4, 0.2)]
 
     def add_constraints(self, prog, N, x, context, single_leg, plant, plant_context):
         world_frame = single_leg.world_frame()
-        # print("world_frame", world_frame)
         base_frame = single_leg.GetFrameByName("toe0")
 
         frame_names = ["toe0", "lower0", "upper0"]
@@ -70,7 +94,6 @@ class Obstacles:
                 toe_xyz = plant_eval.CalcRelativeTransform(context_eval, self.resolve_frame(plant_eval, world_frame),
                                                            self.resolve_frame(plant_eval, self.frame))
                 distance = toe_xyz.translation() - self.obs_xyz
-                # print([distance.dot(distance) ** 0.5])
                 return [distance.dot(distance) ** 0.5]
 
             def resolve_frame(self, plant, F):
